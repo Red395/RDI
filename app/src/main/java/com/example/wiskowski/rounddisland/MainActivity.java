1999f9a247dcd2ac2da060a34ecf86b981bdc706
Raven <ravend2013@gmail.com>
2018-07-09 15:04:51 -0400

    FileReader is working

--- app/src/main/java/com/example/wiskowski/rounddisland/MainActivity.java
+++ app/src/main/java/com/example/wiskowski/rounddisland/MainActivity.java
@@ -1,5 +1,5 @@
 package com.example.wiskowski.rounddisland;
-import android.content.res.AssetManager;
+import android.content.Context;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -7,33 +7,26 @@ import android.widget.TextView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import java.io.IOException;
-
 public class MainActivity extends AppCompatActivity {
-   // Private currentView = R.layout.activity_main;
+
+    Context mContext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-      //  R.layout.activity_main.
+
+        mContext = this;
+
         Button button = (Button) findViewById(R.id.NextButton);
-
-        AssetManager am = getAssets();
-        String[] files = {};
-
-        try { files = am.list("Directory"); }
-        catch (IOException e) {}
-
-        final String[] fileList = files;
-
         button.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View view){
                 TextView text = (TextView)findViewById(R.id.NextText);
-                //FileReaderMechanics test = new FileReaderMechanics();
-                text.setText(fileList[0]);
+                FileReaderMechanics fmReader = new FileReaderMechanics(mContext);
+                text.setText(fmReader.getFiles()[0]);
             }
         });
 
