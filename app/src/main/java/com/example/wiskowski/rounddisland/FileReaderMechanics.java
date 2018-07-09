a552350ea4cc24561a7ad2126a3544e610960ed9
Raven <ravend2013@gmail.com>
2018-07-09 15:04:26 -0400

    FileReader is working

--- app/src/main/java/com/example/wiskowski/rounddisland/FileReaderMechanics.java
+++ app/src/main/java/com/example/wiskowski/rounddisland/FileReaderMechanics.java
@@ -1,34 +1,45 @@
 package com.example.wiskowski.rounddisland;
+import android.content.Context;
+import android.content.res.AssetManager;
+
 import java.io.File;
+import java.io.IOException;
 
 public class FileReaderMechanics {
-    private File[] files; // used to hold all the files
+    private String[] files = {}; // used to hold all the files
     private String dir;     // used to hold the directory
+    private Context mContext;
 
     /** Checks the default directory ("app\src\assets\Directory")
      *  Creates a list of the files in that directory
      */
-    public FileReaderMechanics() {
-        this.dir = dir;
+    public FileReaderMechanics(Context mContext) {
+        AssetManager am = mContext.getAssets();
 
-        File folder = new File("assets/Directory");
-        files = folder.listFiles();
+        try { files = am.list("Directory"); }
+        catch (IOException e) {}
+
+        final String[] fileList = files;
     }
 
     /** Checks the specified directory
      *  @param dir : the directory for the files
      */
-    public FileReaderMechanics(String dir) {
+    public FileReaderMechanics(Context mContext, String dir) {
         this.dir = dir;
 
-        File folder = new File(dir);
-        files = folder.listFiles();
+        AssetManager am = mContext.getAssets();
+
+        try { files = am.list(dir); }
+        catch (IOException e) {}
+
+        final String[] fileList = files;
     }
 
     /** Returns a list of files
      *  @return : the list of files as type File
      */
-    public File[] getFiles() {
+    public String[] getFiles() {
         return files;
     }
 }
