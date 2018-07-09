8642a64b158193abd9dfb9afa6f736692c11a569
Red395 <ssgalloway0@gmail.com>
2018-07-09 13:45:58 -0300

    Added ButtonHandler

--- app/src/main/java/com/example/wiskowski/rounddisland/MainActivity.java
+++ app/src/main/java/com/example/wiskowski/rounddisland/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.wiskowski.rounddisland;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.security.PrivateKey;
 
+public class MainActivity extends AppCompatActivity {
+   // Private currentView = R.layout.activity_main;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Integer temp = 0;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+      //  R.layout.activity_main.
+        Button button = (Button) findViewById(R.id.NextButton);
+        button.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view){
+                TextView text = (TextView)findViewById(R.id.NextText);
+                text.setText("blablabla");
+            }
+        });
+
     }
 }
