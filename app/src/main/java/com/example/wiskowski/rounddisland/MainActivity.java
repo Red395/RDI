b85a7e3f65afdd8c0560d91ddd0ff0ffff5e9478
Red395 <ssgalloway0@gmail.com>
2018-07-10 17:12:09 -0300

    Made Picture Loading work properly

--- app/src/main/java/com/example/wiskowski/rounddisland/MainActivity.java
+++ app/src/main/java/com/example/wiskowski/rounddisland/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.wiskowski.rounddisland;
 import android.content.Context;
-import android.graphics.Picture;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -10,7 +10,6 @@ import android.widget.TextView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import java.io.IOException;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -47,26 +46,30 @@ public class MainActivity extends AppCompatActivity {
         String[] allFileNames = fmReader.getFiles();
         ArrayList<String> FileStringRows= null;
         TableLayout tblLandmarksDisplay = findViewById(R.id.TblAllLandmarks);
+        tblLandmarksDisplay.removeAllViews();
 
         for (String eachFileName : allFileNames) {
             try {
                 FileStringRows = fmReader.getTextFileContents(eachFileName);
                 createImageRow(tblLandmarksDisplay, FileStringRows.get(0), FileStringRows.get(1));
-            }catch (Exception e){ }
-
+            }catch (Exception e){}
         }
     }
 
     private void createImageRow(TableLayout tbl,String Name, String PictureName){
         try {
-            TextView text = (TextView)findViewById(R.id.NextText);
-            text.setText(Name+ PictureName);
             TableRow row = new TableRow(this);
             TextView rowText = new TextView(this);
+            ImageView rowImg = new ImageView(this);
             rowText.setText(Name);
+            int imgId = rowImg.getContext().getResources().getIdentifier(PictureName, "drawable", rowImg.getContext().getPackageName());
+            rowImg.setImageResource(imgId);
             row.addView(rowText);
+            row.addView(rowImg);
             tbl.addView(row);
-
-        } catch (Exception e) {}
+            rowImg.requestLayout();
+            rowImg.getLayoutParams().height = 400;
+            rowImg.getLayoutParams().width = 400;
+        } catch (Exception e) {   }
     }
 }
