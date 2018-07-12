34d5efd38cf420fefd0d0606f4d66b06c1a19165
Raven <ravend2013@gmail.com>
2018-07-12 16:45:27 -0400

    Working QR Reader Refactored MainActivity to HomePage HomePage Scan button takes user to QRReader class HomePage Let's Play Button should be used to test (currently points to Directory class) Added ZXing to the build.gradle

--- app/src/main/java/com/example/wiskowski/rounddisland/HomePage.java
+++ app/src/main/java/com/example/wiskowski/rounddisland/HomePage.java
@@ -8,36 +8,39 @@ import android.view.View;
 import android.widget.Button;
 
 public class HomePage extends AppCompatActivity {
+    Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.homepage);
 
-        Context mContext = this;
+        mContext = this;
+        scanCodeButton();
+        startGameButton();
     }
 
     private void scanCodeButton() {
-        Button button = (Button) findViewById(R.id.ScanCodeButton);
-        button.setOnClickListener(new View.OnClickListener(){
+        Button button = findViewById(R.id.ScanCodeButton);
+        button.setOnClickListener(new View.OnClickListener() {
 
             @Override
-            public void onClick(View view){
-                Button button = (Button) view;
+            public void onClick(View view) {
                 // change "HomePage.class" to the desired class
-                startActivity(new Intent(getApplicationContext(), HomePage.class));
+                startActivity(new Intent(getApplicationContext(), QRReader.class));
             }
         });
     }
 
+    // TODO: 7/12/2018   Change Directory.class to main game page
     private void startGameButton() {
-        Button button = (Button) findViewById(R.id.StartGameButton);
+        Button button = findViewById(R.id.StartGameButton);
         button.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View view){
                 Button button = (Button) view;
-                startActivity(new Intent(getApplicationContext(), HomePage.class));
+                startActivity(new Intent(getApplicationContext(), Directory.class)); // change to main game page
             }
         });
     }
---
+++ app/src/main/java/com/example/wiskowski/rounddisland/QRReader.java
@@ -0,0 +1,81 @@
+package com.example.wiskowski.rounddisland;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class QRReader extends AppCompatActivity implements View.OnClickListener {
+    // view objects
+    private Button ScanCodeButton;
+    private TextView TestTextUpdate;
+
+    private String updateText;
+
+    // qr reader
+    private IntentIntegrator qrScan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // checks to see if the app has camera permissions
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, 1);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.scancode);
+
+        ScanCodeButton = findViewById(R.id.ScanCodeButton);
+        TestTextUpdate = findViewById(R.id.TestText);
+
+        ScanCodeButton.setOnClickListener(this);
+
+        //ImageView qrViewer = findViewById(R.id.qrCode);
+
+        qrScan = new IntentIntegrator(this);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() == null) {
+                Toast.makeText(this, "No QR Code Found", Toast.LENGTH_LONG).show();
+            } else {
+                try {
+                    JSONObject obj = new JSONObject(result.getContents());
+                    //TestTextUpdate.setText(obj.getString("name"));
+                    updateText = result.getContents();
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    updateText = result.getContents();
+                    //TestTextUpdate.setText(result.getContents());
+                    //Toast.makeText(this, result.getContents(), Toast.LENGTH_LONG).show();
+                }
+
+                TestTextUpdate.setText(updateText);
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        qrScan.initiateScan();
+    }
+}
