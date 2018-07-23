2b22dd4a1078e197c367e0af98372125cfed048c
Raven <ravend2013@gmail.com>
2018-07-23 17:26:41 -0400

    QRReader stripped Camera permissions placed into HomePage QRReader xml page removed Homepage - QRReader call altered

--- app/src/main/java/com/example/wiskowski/rounddisland/HomePage.java
+++ app/src/main/java/com/example/wiskowski/rounddisland/HomePage.java
@@ -1,20 +1,33 @@
 package com.example.wiskowski.rounddisland;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.zxing.integration.android.IntentIntegrator;
+
 public class HomePage extends AppCompatActivity {
+    private IntentIntegrator qrScan;
     Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // needed to check for permissions
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, 1);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.homepage);
 
+        // new scanner object
+        qrScan = new IntentIntegrator(this);
         mContext = this;
         scanCodeButton();
         startGameButton();
@@ -26,8 +39,8 @@ public class HomePage extends AppCompatActivity {
 
             @Override
             public void onClick(View view) {
-                // change "HomePage.class" to the desired class
-                startActivity(new Intent(getApplicationContext(), QRReader.class));
+                // run the qrScanner
+                qrScan.initiateScan();
             }
         });
     }
@@ -42,5 +55,11 @@ public class HomePage extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), Directory.class)); // change to main game page
             }
         });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        QRReader codeRunner = new QRReader(); // calls the QRReader class to deal with the result of the scan
+        codeRunner.onActivityResult(requestCode, resultCode, data, this);
     }
 }
--- app/src/main/java/com/example/wiskowski/rounddisland/QRReader.java
+++ app/src/main/java/com/example/wiskowski/rounddisland/QRReader.java
@@ -1,47 +1,20 @@
 package com.example.wiskowski.rounddisland;
 
-import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
-public class QRReader extends AppCompatActivity implements View.OnClickListener {
-    // view objects
-    private Button ScanCodeButton;
+public class QRReader extends AppCompatActivity {
 
-    // qr reader
-    private IntentIntegrator qrScan;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        // checks to see if the app has camera permissions
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
-            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, 1);
-
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.homepage);
-
-        ScanCodeButton = findViewById(R.id.ScanCodeButton);
-        ScanCodeButton.setOnClickListener(this);
-
-        qrScan = new IntentIntegrator(this);
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    public void onActivityResult(int requestCode, int resultCode, Intent data, Context page) {
         IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
         if (result != null) {
             if (result.getContents() == null) {
-                Toast.makeText(this, "No QR Code Found", Toast.LENGTH_LONG).show();
+                Toast.makeText(page, "No QR Code Found", Toast.LENGTH_LONG).show();
             } else {
                 try {
                     String testText = result.getContents(); // tries to convert the result to a string
@@ -54,10 +27,5 @@ public class QRReader extends AppCompatActivity implements View.OnClickListener 
         } else {
             super.onActivityResult(requestCode, resultCode, data);
         }
-    }
-
-    @Override
-    public void onClick(View view) {
-        qrScan.initiateScan();
     }
 }
