5d31ee145c76a6d5f960ff26ef2801b477724ed7
Raven <ravend2013@gmail.com>
2018-08-12 14:22:01 -0400

    Added the winpage

--- app/src/main/java/com/example/wiskowski/rounddisland/WinPage.java
+++ app/src/main/java/com/example/wiskowski/rounddisland/WinPage.java
@@ -2,14 +2,22 @@ package com.example.wiskowski.rounddisland;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 
 import pl.droidsonroids.gif.GifTextView;
 
 public class WinPage extends AppCompatActivity {
+    private GifTextView gif;
+
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
@@ -18,11 +26,48 @@ public class WinPage extends AppCompatActivity {
         getSupportActionBar().setDisplayShowTitleEnabled(false);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setIcon(R.drawable.rdihorizontalwhite);
+
+        timer();
     }
 
     private void spawnFireworks() {
-        GifTextView gif = new GifTextView(this);
-        //gif.setX();
+        gif = new GifTextView(this);
+        gif.setBackgroundResource(R.drawable.fireworks);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        int screenX = dm.widthPixels;
+        int screenY = dm.heightPixels;
+
+        gif.setX((int)((Math.random() * screenX) * .8));
+        gif.setY((int)((Math.random() * screenY) * .8));
+        gif.setScaleX(1);
+        gif.setScaleY(1);
+
+        gif.setVisibility(View.VISIBLE);
+
+        RelativeLayout sv = findViewById(R.id.rlwinpage);
+        sv.addView(gif);
+    }
+
+    private void removeFirework() {
+        if (gif != null) {
+            RelativeLayout sv = findViewById(R.id.rlwinpage);
+            sv.removeView(gif);
+        }
+    }
+
+    private void timer() {
+        final Handler handler = new Handler();
+        final int delay = 1000; //milliseconds
+
+        handler.postDelayed(new Runnable(){
+            public void run(){
+                removeFirework();
+                spawnFireworks();
+                handler.postDelayed(this, delay);
+            }
+        }, delay);
     }
 
     @Override
